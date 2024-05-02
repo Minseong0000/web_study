869929467ce21db25a298f206f179048c47e7208
Minseong0000 <dave711@naver.com>
2024-05-02 16:56:02 +0900

    study update

---
+++ 240501_html/문제.js
@@ -0,0 +1,80 @@
+
+/* let ran = Math.ceil(Math.random() * 9);
+ // console.log(ran);
+
+ const tds = document.querySelectorAll('td');
+ console.log(tds);
+
+ tds.forEach((td, idx) => {
+   // document.td.innerText = ran;
+   td.textContent = `${ran}`;
+
+ });
+*/
+var usedNums = new Array();
+
+function newCard() {
+  for (var i = 0; i < 9; i++) {
+    setSquare(i);
+  }
+}
+// console.log();
+
+function setSquare(squareNumber) {  
+  var currSquare = "square" + squareNumber;
+  var newNum;
+
+  do {
+    newNum = getNewNum() + 1;
+  }
+  while (usedNums[newNum]);
+
+  usedNums[newNum] = true;
+  document.getElementById(currSquare).innerHTML = newNum;
+}
+console.log(usedNums);
+
+
+
+function getNewNum() {
+  return Math.floor(Math.random() * 9);
+}
+
+function anotherCard() {
+  for (var i = 1; i < usedNums.length; i++) {
+    usedNums[i] = false;
+  }
+
+  newCard();
+}
+
+function randomNumber() {
+  return console.log(Math.ceil(Math.random()*9));
+/*   if(ran == newNum) {
+    currSquare.backgroundColor = 'blue';
+  } */
+}
+
+/* function winNum() {
+  var callNum;
+
+  do {
+    callNum = randomNumber();
+  }
+  while (usedNum[callNum]);
+
+  usedNums[callNum] = true;
+
+  if(callNum = newNum) {
+    newCard().style.backgroundColor = 'orange';
+  }
+}
+
+winNum(); */
+// if(randomNumber()==)
+
+
+/* if(randomNumber() == newNum) {
+  
+} */
+// console.log(setSquare());
